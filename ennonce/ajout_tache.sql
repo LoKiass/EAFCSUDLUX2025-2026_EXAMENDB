c6796d0aaf441418062dff17c9354c1f59274980
Mattéo Diseurs <diseursmat@gmail.com>
2025-12-10 19:30:23 +0100

    Modification avancé de la table Task, avec Restructuration rapide de fonction get_hierachy
    Ajout de la stor proc ajout_tache dans ajout_tache.sql
    
    UPDATE EN COURS : MODIFICATION DE LA DOC + INFORMATION RETOUR DE LA FONCTION get_hierarchy()

---
+++ ennonce/ajout_tache.sql
@@ -0,0 +1,21 @@
+CREATE OR REPLACE PROCEDURE ajout_tache (nom_tache TEXT,temps_estimer INT,tache_parent TEXT)
+AS $$
+DECLARE
+    parent_id INT;
+BEGIN
+
+    IF tache_parent IS NOT NULL THEN
+        SELECT task_id INTO parent_id
+        FROM Task
+        WHERE nom_task = tache_parent;
+
+        INSERT INTO Task (parent_id, nom_task, description_task, temp_tache)
+        VALUES (parent_id, nom_tache, ' ', temps_estimer);
+
+    ELSE
+        INSERT INTO examen_matteodiseurs.public.task (nom_task, description_task, temp_tache)
+        VALUES (nom_tache, ' ', temps_estimer);
+    END IF;
+
+END;
+$$ LANGUAGE plpgsql;
